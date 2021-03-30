83c783bb8b01331269e33d1cd0153eb89fafb230
Suyash S <suyash988@gmail.com>
2021-03-30 19:50:39 -0400

    fixed eslint href # error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Header from './Header';
+
 function App() {
   return (
     <Router>
-      <div>Placeholder</div>
+      <Header />
       <Switch>
         <Route path="/" />
         <Route path="/search" />
