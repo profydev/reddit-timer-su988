eece31c818236dd9263b1f45c1ed7b6ae8892329
Su988 <suyash988@gmail.com>
2021-03-30 11:58:14 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import Header from './Header';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
-    <Router>
-      <div>Placeholder</div>
-      <Switch>
-        <Route path="/" />
-        <Route path="/search" />
-      </Switch>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Header />
+        <Switch>
+          <Route path="/">Home</Route>
+          <Route path="/search">Search</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
