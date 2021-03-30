b509a87880f0652241c698b9ad099cb109693922
Su988 <suyash988@gmail.com>
2021-03-30 16:55:55 -0400

    logo and navbar added

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header>
-      <nav>a</nav>
+      <nav>
+        <Link to="/">
+          <img src="./logo.svg" alt="Logo" />
+        </Link>
+        <ul>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <Link to="#how-it-works">How it works</Link>
+          </li>
+          <li>
+            <Link to="#about">About</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
