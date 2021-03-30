2284fd646717dda298ed1d29a4ea4119d7defb12
Su988 <suyash988@gmail.com>
2021-03-30 11:31:12 -0400

    fixed failing test for Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
+      <div>Placeholder</div>
       <Switch>
         <Route path="/" />
         <Route path="/search" />
       </Switch>
-    </>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
