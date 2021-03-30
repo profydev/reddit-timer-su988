42650989e70db7cedf561b4d4c859fcb752302f9
Su988 <suyash988@gmail.com>
2021-03-30 12:00:40 -0400

    added header component.. removed theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+// import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
 import Header from './Header';
-import GlobalStyle from './GlobalStyle';
+// import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <Normalize />
-        <GlobalStyle />
-        <Header />
-        <Switch>
-          <Route path="/">Home</Route>
-          <Route path="/search">Search</Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Normalize />
+      <Header />
+      <Switch>
+        <Route path="/">Home</Route>
+        <Route path="/search">Search</Route>
+      </Switch>
+    </Router>
   );
 }
 
