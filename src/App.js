64a77b2fac1181355ad97ac99dd1ec45363efafa
Suyash S <su988@users.noreply.github.com>
2021-04-07 11:20:06 -0400

    Info section (#7)
    * added css reset, fonts and setup react router
    
    * fixed failing test for Router
    
    * added logo and links in header
    
    * fixed eslint href # error
    
    * header logo nav
    
    * styled component header
    
    * footer added
    
    * home route exact
    
    * link error footer retry
    
    * code review changes
    
    * title, subtitle, button n subreddit added to Hero
    
    * hero img and Link added
    
    * info section added
    
    * scroll to id
    
    * changed id
    
    * changed id edit 2
    
    * footer margin
    
    * changed test windowscroll 100 to 20

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Hero from './Hero';
+import Info from './Info';
 import Footer from './Footer';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Footer />
       <Switch>
         <Route exact path="/">
           <Hero />
+          <Info />
         </Route>
         <Route path="/search" />
         <Route path="/terms" />
